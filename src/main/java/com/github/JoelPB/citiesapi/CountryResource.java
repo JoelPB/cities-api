e0091a39ab9f0ee08dcbf231e8b6aad15e889e2c
JoelPB <coffee.program.joel@gmail.com>
2021-08-10 13:33:54 -0300

    Até a parte 6

--- src/main/java/com/github/JoelPB/citiesapi/CountryResource.java
+++ src/main/java/com/github/JoelPB/citiesapi/CountryResource.java
@@ -1,16 +1,26 @@
 package com.github.JoelPB.citiesapi;
 
 import com.github.JoelPB.citiesapi.countries.Country;
+import com.github.JoelPB.citiesapi.repository.CountryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/countries")
 public class CountryResource {
 
+    private CountryRepository repository;
+
+    public CountryResource(CountryRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping
     public List<Country> countries(){
-        return
+        return repository.findAll();
     }
 }
