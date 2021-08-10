251ded65f73d0fa0acdb820eb851bada62669bc6
JoelPB <coffee.program.joel@gmail.com>
2021-08-10 14:24:40 -0300

    Até a parte 8

--- src/main/java/com/github/JoelPB/citiesapi/CountryResource.java
+++ src/main/java/com/github/JoelPB/citiesapi/CountryResource.java
@@ -2,11 +2,15 @@ package com.github.JoelPB.citiesapi;
 
 import com.github.JoelPB.citiesapi.countries.Country;
 import com.github.JoelPB.citiesapi.repository.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -19,8 +23,24 @@ public class CountryResource {
         this.repository = repository;
     }
 
+//    @GetMapping
+//    public List<Country> countries(){
+//        return repository.findAll();
+//    }
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
